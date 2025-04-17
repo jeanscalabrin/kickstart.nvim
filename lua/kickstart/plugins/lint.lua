33e21c30dd7461ae88e7af97235b184068f90f33
Jean Scalabrin <jscalabrinn@gmail.com>
2025-04-17 10:29:48 -0300

    change theme and lint

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,8 +7,10 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
-        typescript = { 'eslint' },
-        javascript = { 'eslint' },
+        typescript = { 'eslint_d' },
+        javascript = { 'eslint_d' },
+        javascriptreact = { 'eslint_d' },
+        typescriptreact = { 'eslint_d' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
